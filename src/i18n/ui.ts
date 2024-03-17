1411278af229aa267462da0d4e735db26c7d3172
Gonzalo09 <gonza.iglesias00@gmail.com>
2024-03-17 18:48:29 -0300

    Primer intento de traducciones

---
+++ src/i18n/ui.ts
@@ -0,0 +1,112 @@
+export const languages = {
+  es: "Español",
+  en: "English",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  es: {
+    "nav.experiencia": "Experiencia",
+    "nav.proyectos": "Proyectos",
+    "nav.habilidades": "Habilidades",
+    "nav.contacto": "Contacto",
+    "inicio.titulo": "Hola, soy",
+    "inicio.titulo.span": "Gonzalo",
+    "inicio.parrafo.1": "Estudiante de informática y ",
+    "inicio.parrafo.span.1": "desarrollador Frontend ",
+    "inicio.parrafo.2":
+      "con +1 año de experiencia creando aplicaciones web que sean ",
+    "inicio.parrafo.span.2": "rápidas, accesibles y atractivas.",
+    "inicio.parrafo.3":
+      " Me especializo en el desarrollo de interfaces de usuario y en la implementación de diseños de alta calidad.",
+    "inicio.curriculum": "Currículum",
+    "experiencia.titulo": "Experiencia",
+    "experiencia.unahur.puesto": "Desarrollador",
+    "experiencia.unahur.fecha": "Marzo, 2023 - Diciembre, 2023",
+    "experiencia.unahur.descripcion":
+      "Participación en el desarrollo de la bolsa de trabajo de la UNAHUR. Incorporación de nuevas funcionalidades, refactorización de código y rediseño de la interfaz. Tecnologías utilizadas: React, MUI, Node.js, Express, PostgreSQL. Metodologías ágiles.",
+    "proyectos.titulo": "Proyectos",
+    "proyecto.bolsa.unahur.titulo": "Bolsa de empleo UNAHUR",
+    "proyecto.bolsa.unahur.descripcion":
+      "Bolsa de empleo para la comunidad de la Universidad Nacional de Hurlingham. Esta aplicación permite a los usuarios registrarse y postularse a las ofertas de trabajo publicadas por las empresas. El administrador puede gestionar las ofertas de trabajo y los datos de los usuarios.",
+    "proyecto.chat.titulo": "Equivalencias UNAHUR",
+    "proyecto.chat.descripcion":
+      "Gestor de pedidos de equivalencias para la Universidad Nacional de Hurlingham al cual se le incorporó la funcionalidad de chat para poder comunicarse con los alumnos.",
+    "proyecto.rick.titulo": "Rick and Morty App",
+    "proyecto.rick.descripcion":
+      "Rick and Morty App es una aplicación web que permite visualizar información sobre la serie animada Rick and Morty. En esta encontrarás información sobre los personajes, episodios y locaciones de la famosa serie.",
+    "proyecto.clima.titulo": "Clima Argentina",
+    "proyecto.clima.descripcion":
+      "Aplicación web que permite visualizar las condiciones climáticas de cualquier ciudad de Argentina. Además de proporcionar datos del clima actual, como visibilidad, humedad, presión, etc, también podrás consultar el pronóstico extendido de 3 días.",
+    "proyectos.boton.codigo": "Código",
+    "habilidades.titulo": "Habilidades",
+    "habilidades.aprendiendo": "Aprendiendo",
+    "habilidades.herramientas": "Herramientas",
+    "contacto.titulo": "Contacto",
+    "contacto.parrafo.1":
+      "Si tenes alguna duda o sugerencia, no dudes en dejarla en el siguiente formulario de contacto. Estaré encantado de leerte y responder a la mayor brevedad posible.",
+    "contacto.parrafo.2":
+      "También podes hacerlo directamente a mi correo electrónico ",
+    "contacto.nombre": "Nombre",
+    "contacto.nombre.placeholder": "Su nombre",
+    "contacto.email": "Email",
+    "contacto.email.placeholder": "Su email",
+    "contacto.mensaje": "Mensaje",
+    "contacto.mensaje.placeholder": "Escriba su mensaje aquí",
+    "contacto.enviar": "Enviar mensaje",
+    "footer.texto": "¡Gracias por visitar mi portafolio! 🚀",
+    "footer.repositorio": "Repositorio",
+  },
+  en: {
+    "nav.experiencia": "Experience",
+    "nav.proyectos": "Projects",
+    "nav.habilidades": "Skills",
+    "nav.contacto": "Contact",
+    "inicio.titulo": "Hi, I'm",
+    "inicio.titulo.span": "Gonzalo",
+    "inicio.parrafo.1": "Computer science student and ",
+    "inicio.parrafo.span.1": "Frontend developer ",
+    "inicio.parrafo.2":
+      "with +1 year of experience creating web applications that are ",
+    "inicio.parrafo.span.2": "fast, accessible and attractive.",
+    "inicio.parrafo.3":
+      " I specialize in the development of user interfaces and the implementation of high-quality designs.",
+    "inicio.curriculum": "Resume",
+    "experiencia.titulo": "Experience",
+    "experiencia.unahur.puesto": "Developer",
+    "experiencia.unahur.fecha": "March, 2023 - December, 2023",
+    "experiencia.unahur.descripcion":
+      "Participation in the development of the UNAHUR job board. Incorporation of new functionalities, code refactoring and interface redesign. Technologies used: React, MUI, Node.js, Express, PostgreSQL. Agile methodologies.",
+    "proyectos.titulo": "Projects",
+    "proyecto.bolsa.unahur.titulo": "UNAHUR Job Board",
+    "proyecto.bolsa.unahur.descripcion":
+      "Job board for the Universidad Nacional de Hurlingham community. This application allows users to register and apply for job offers published by companies. The administrator can manage job offers and user data.",
+    "proyecto.chat.titulo": "UNAHUR Equivalences",
+    "proyecto.chat.descripcion":
+      "Equivalences manager for the Universidad Nacional de Hurlingham to which chat functionality was added to communicate with students.",
+    "proyecto.rick.titulo": "Rick and Morty App",
+    "proyecto.rick.descripcion":
+      "Rick and Morty App is a web application that allows you to view information about the animated series Rick and Morty. Here you will find information about the characters, episodes and locations of the famous series.",
+    "proyecto.clima.titulo": "Argentina Weather",
+    "proyecto.clima.descripcion":
+      "Web application that allows you to view the weather conditions of any city in Argentina. In addition to providing data on the current weather, such as visibility, humidity, pressure, etc., you can also check the extended forecast for 3 days.",
+    "proyectos.boton.codigo": "Code",
+    "habilidades.titulo": "Skills",
+    "habilidades.aprendiendo": "Learning",
+    "habilidades.herramientas": "Tools",
+    "contacto.titulo": "Contact",
+    "contacto.parrafo.1":
+      "If you have any questions or suggestions, do not hesitate to leave them in the following contact form. I will be happy to read and respond as soon as possible.",
+    "contacto.parrafo.2": "You can also do it directly to my email ",
+    "contacto.nombre": "Name",
+    "contacto.nombre.placeholder": "Your name",
+    "contacto.email": "Email",
+    "contacto.email.placeholder": "Your email",
+    "contacto.mensaje": "Message",
+    "contacto.mensaje.placeholder": "Write your message here",
+    "contacto.enviar": "Send message",
+    "footer.texto": "Thanks for visiting my portfolio! 🚀",
+    "footer.repositorio": "Repository",
+  },
+} as const;
